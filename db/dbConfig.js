ffe978e17156d552f343db9020bbafda937ce54b
eivor9 <njeremiah@pursuit.org>
2024-09-23 12:23:56 -0400

    create initial db

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -2,19 +2,9 @@
 const pgp = require("pg-promise")();
 require("dotenv").config();
 
-const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD } =
-  process.env;
-// https://github.com/vitaly-t/pg-promise/wiki/Connection-Syntax#configuration-object
-const cn = DATABASE_URL
-  ? {
-      connectionString: DATABASE_URL,
-      max: 30,
- // this key value is only required for heroku deployment
-//       ssl: {
-//         rejectUnauthorized: false,
-//       },
-    }
-  : {
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD } = process.env;
+const cn = 
+    {
       host: PG_HOST,
       port: PG_PORT,
       database: PG_DATABASE,
--- db/prod_schema.sql
+++ db/prod_schema.sql
@@ -1,6 +1,39 @@
-DROP TABLE IF EXISTS test;
+DROP DATABASE IF EXISTS mvp_dev;
+CREATE DATABASE mvp_dev;
 
-CREATE TABLE test (
-    id SERIAL PRIMARY KEY, 
-    name TEXT
+\c mvp_dev;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS subcategories;
+DROP TABLE IF EXISTS connections;
+DROP TABLE IF EXISTS assignments;
+DROP TABLE IF EXISTS conversations;
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS metrics;
+
+CREATE TABLE users (
+
 );
+CREATE TABLE categories (
+
+);
+CREATE TABLE subcategories (
+
+);
+CREATE TABLE connections (
+
+);
+CREATE TABLE assignments (
+
+);
+CREATE TABLE conversations (
+
+);
+CREATE TABLE messages (
+
+);
+CREATE TABLE metrics (
+
+);
+
