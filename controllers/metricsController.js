1019c98b00154d575f49fadd594c8be1424e3105
JayZ44 <jazonyounge@pursuit.org>
2024-09-26 15:10:56 -0400

    metrics queries and controllers done

--- controllers/metricsController.js
+++ controllers/metricsController.js
@@ -1,30 +1,59 @@
 // DEPENDENCIES
 const express = require("express");
 const metrics = express.Router({ mergeParams: true });
+const {
+  getAllMetrics,
+  getMetric,
+  createMetric,
+  deleteMetric,
+  updateMetric,
+} = require("../queries/metrics");
 
 // INDEX
-metrics.get("/", async (req, res) => {
-
+metrics.get("/all", async (req, res) => {
+  const allMetrics = await getAllMetrics();
+  if (allMetrics[0]) {
+    res.status(200).json(allMetrics);
+  } else {
+    res.status(500).json({ error: "server error" });
+  }
 });
 
 // SHOW
 metrics.get("/:id", async (req, res) => {
-    
+  const metric = await getMetric();
+  if (metric) {
+    res.json(metric);
+  } else {
+    res.status(400).json({ error: "not found" });
+  }
 });
 
 // CREATE
 metrics.post("/", async (req, res) => {
-    
+  const newMetric = await createMetric(req.body);
+  res.json(newMetric);
 });
 
 // DELETE
 metrics.delete("/:id", async (req, res) => {
-
+  const deletedMetric = await deleteMetric();
+  if (deletedMetric) {
+    res.json(deletedMetric);
+  } else {
+    res.status(400).json({ error: "not found" });
+  }
 });
 
 // UPDATE
 metrics.put("/:id", async (req, res) => {
-    
+  const { id } = req.params;
+  const updatingMetric = await updateMetric(id, req.body);
+  if (updatingMetric.id) {
+    res.status(200).json(updatingMetric);
+  } else {
+    res.status(404).json({ error: "not found. oof" });
+  }
 });
 
 module.exports = metrics;
--- queries/metrics.js
+++ queries/metrics.js
@@ -1,43 +1,72 @@
 const db = require("../db/dbConfig.js");
 
+//Contact Jazon if error found.
+
 const getAllMetrics = async () => {
   try {
-    
+    const allMetrics = await db.any("SELECT * FROM metrics");
+    console.log(allMetrics);
+    return allMetrics;
   } catch (error) {
     return error;
   }
 };
 
-const getMetric = async () => {
+const getMetric = async (id) => {
   try {
-    
+    const metrics = await db.one("SELECT * FROM metrics WHERE id=$1", id);
+    // console.log(metrics);
+    return metrics;
   } catch (error) {
     return error;
   }
 };
 
-const createMetric = async () => {
+const createMetric = async (metric) => {
+  const { name, connection_id, progress } = metric;
   try {
-
+    const newMetric = await db.one(
+      "INSERT INTO metrics (name,connection_id,progress) VALUES($1,$2,$3) RETURNING *",
+      [name, connection_id, progress]
+    );
+    // console.log(newMetric)
+    return newMetric;
   } catch (error) {
     return error;
   }
 };
 
-const deleteMetric = async () => {
+const deleteMetric = async (id) => {
   try {
-
+    const deletedMetric = await db.any(
+      "DELETE FROM metrics WHERE id=$1 RETURNING *",
+      id
+    );
+    // console.log(deleteMetric)
+    return deletedMetric;
   } catch (error) {
     return error;
   }
 };
 
-const updateMetric = async () => {
+const updateMetric = async (id, metric) => {
+  const { name, connection_id, progress } = metric;
   try {
-    
+    const updatedMetric = await db.one(
+      "UPDATE metrics SET name=$1, connection_id=$2, progress=$3 WHERE id=$4 RETURNING *",
+      [name, connection_id, progress, id]
+    );
+    // console.log(updatedMetric)
+    return updatedMetric;
   } catch (error) {
     return error;
   }
 };
 
-module.exports = { getAllMetrics, getMetric, createMetric, deleteMetric, updateMetric };
+module.exports = {
+  getAllMetrics,
+  getMetric,
+  createMetric,
+  deleteMetric,
+  updateMetric,
+};
