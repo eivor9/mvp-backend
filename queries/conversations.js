1161fe108b58361a5156dbb06b86cbe60c72e50c
jmoncayo-pursuit <jmoncayo@pursuit.org>
2024-09-26 19:16:10 -0400

    Add database queries for managing conversations: get, create, delete

--- queries/conversations.js
+++ queries/conversations.js
@@ -1,27 +1,65 @@
-const db = require("../db/dbConfig.js");
+// queries/conversations.js
+const db = require('../db/dbConfig.js');
 
-const getConversation = async () => {
+// Get all conversations for a specific connection
+const getConversations = async (connection_id) => {
   try {
-    
+    const conversations = await db.any(
+      'SELECT * FROM conversations WHERE connection_id=$1',
+      connection_id
+    );
+    return conversations;
   } catch (error) {
     return error;
   }
 };
 
-const createConversation = async () => {
-    try {
-  
-    } catch (error) {
-      return error;
-    }
-};
-
-const deleteConversation = async () => {
+// Get one specific conversation
+const getOneConversation = async (id) => {
   try {
-    
+    const conversation = await db.one(
+      'SELECT * FROM conversations WHERE id=$1',
+      id
+    );
+    return conversation;
   } catch (error) {
     return error;
   }
 };
 
-module.exports = { getConversation, createConversation, deleteConversation };
+// Create a new conversation
+const createConversation = async (conversation) => {
+  try {
+    const newConversation = await db.one(
+      'INSERT INTO conversations (connection_id, mentor_id, mentee_id) VALUES($1, $2, $3) RETURNING *',
+      [
+        conversation.connection_id,
+        conversation.mentor_id,
+        conversation.mentee_id,
+      ]
+    );
+    return newConversation;
+  } catch (error) {
+    return error;
+  }
+};
+
+// Delete a conversation
+const deleteConversation = async (id) => {
+  try {
+    const deletedConversation = await db.one(
+      'DELETE FROM conversations WHERE id=$1 RETURNING *',
+      id
+    );
+    return deletedConversation;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = {
+  getConversations,
+  getOneConversation,
+  createConversation,
+  deleteConversation,
+};
