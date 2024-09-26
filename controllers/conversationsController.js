c46b3d0219166f8f52024b3d90948917d94a3183
jmoncayo-pursuit <jmoncayo@pursuit.org>
2024-09-26 18:52:50 -0400

    Implement full conversationsController with routes for conversations within connections

--- controllers/conversationsController.js
+++ controllers/conversationsController.js
@@ -1,25 +1,69 @@
+// controllers/conversationsController.js
+
 // DEPENDENCIES
-const express = require("express");
+const express = require('express');
 const conversations = express.Router({ mergeParams: true });
+const {
+  getConversations,
+  getOneConversation,
+  createConversation,
+  deleteConversation,
+} = require('../queries/conversations.js');
 
-// MESSAGES 
-const messagesController = require("./messagesController.js");
-conversations.use("/:conversation_id/messages", messagesController);
+// MESSAGES
+const messagesController = require('./messagesController.js');
+conversations.use('/:conversation_id/messages', messagesController);
 
-
-// SHOW
-conversations.get("/", async (req, res) => {
-    
+// INDEX - Get all conversations for a connection
+conversations.get('/', async (req, res) => {
+  const { connection_id } = req.params;
+  const conversationsList = await getConversations(connection_id);
+  if (conversationsList.length) {
+    res.status(200).json(conversationsList);
+  } else {
+    res
+      .status(500)
+      .json({ error: 'No conversations found or server error' });
+  }
 });
 
-// CREATE
-conversations.post("/", async (req, res) => {
-    
+// SHOW - Get one specific conversation
+conversations.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const conversation = await getOneConversation(id);
+  if (conversation.id) {
+    res.status(200).json(conversation);
+  } else {
+    res
+      .status(500)
+      .json({ error: 'Conversation not found or server error' });
+  }
 });
 
-// DELETE
-conversations.delete("/:id", async (req, res) => {
+// CREATE - Create a new conversation
+conversations.post('/', async (req, res) => {
+  try {
+    const newConversation = await createConversation(req.body);
+    res.status(200).json(newConversation);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
+// DELETE - Delete a conversation
+conversations.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const deletedConversation = await deleteConversation(id);
+  if (deletedConversation.id) {
+    res
+      .status(200)
+      .json({
+        message: 'Conversation successfully deleted',
+        ...deletedConversation,
+      });
+  } else {
+    res.status(404).json({ error: 'Conversation not found' });
+  }
 });
 
 module.exports = conversations;
