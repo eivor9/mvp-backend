de90c50edcbd275aad0fb8119aa83e472b3f3762
jmoncayo-pursuit <jmoncayo@pursuit.org>
2024-10-11 12:39:20 -0400

    Finalize profile page changes

--- validations/userValidations.js
+++ validations/userValidations.js
@@ -19,7 +19,6 @@ const checkLastName = (req, res, next) => {
   // Validate email format
 const validateEmail = (req, res, next) => {
     const { email } = req.body;
-    console.log('Email being validated:', email); // Log the email being validated
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!email || !emailRegex.test(email)) {
       return res.status(400).json({ error: 'A valid email address is required' });
